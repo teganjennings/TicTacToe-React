c10e4aa2a36e305e38bb1c9fb8dc79732acef95a
Tegan Jennings <teganjennings1@hotmail.com>
2024-02-18 00:54:43 +0000

    Adding Player functionality, name changing and saving

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,32 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+	const [playerName, setPlayerName] = useState(initialName);
+	const [isEditing, setIsEditing] = useState(false);
+
+	function handleEditClick() {
+		// best practice for React >> dont use setIsEditing(!isEditing); as it might not grab the most recent code
+		setIsEditing((editing) => !editing);
+	}
+	function handleChange(event) {
+		setPlayerName(event.target.value);
+	}
+
+	let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+	if (isEditing) {
+		editablePlayerName = (
+			<input type="text" required value={playerName} onChange={handleChange} />
+		);
+	}
+
 	return (
 		<li>
 			<span className="player">
-				<span className="player-name">{name}</span>
+				{editablePlayerName}
 				<span className="player-symbol">{symbol}</span>
 			</span>
-			<button>Edit</button>
+			<button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
 		</li>
 	);
 }
