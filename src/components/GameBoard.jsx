b1c98c15f4bd4ac9e5ae1d5bb9c56c03f57f8532
Tegan Jennings <teganjennings1@hotmail.com>
2024-02-21 20:29:51 +0000

    Adding Winning Functionality

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,33 +1,17 @@
-// import { useState } from "react";
-
-const initialGameBoard = [
-	[null, null, null],
-	[null, null, null],
-	[null, null, null],
-];
-export default function GameBoard({ onSelectSquare }) {
-	// const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-	// function handleSelectSquare(rowIndex, colIndex) {
-	// 	setGameBoard((prevGameBoard) => {
-	// 		const updatedBoard = [
-	// 			...prevGameBoard.map((innerArray) => [...innerArray]),
-	// 		];
-	// 		updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-	// 		return updatedBoard;
-	// 	});
-
-	// 	onSelectSquare();
-	// }
-
+export default function GameBoard({ onSelectSquare, board }) {
 	return (
 		<ol id="game-board">
-			{gameBoard.map((row, rowIndex) => (
+			{board.map((row, rowIndex) => (
 				<li key={rowIndex}>
 					<ol>
 						{row.map((playerSymbol, colIndex) => (
 							<li key={colIndex}>
-								<button onClick={onSelectSquare}>{playerSymbol}</button>
+								<button
+									onClick={() => onSelectSquare(rowIndex, colIndex)}
+									disabled={playerSymbol !== null}
+								>
+									{playerSymbol}
+								</button>
 							</li>
 						))}
 					</ol>
