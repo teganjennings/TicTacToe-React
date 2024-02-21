ba2b9ad54f30e2968148ab815699a483fcb2add4
Tegan Jennings <teganjennings1@hotmail.com>
2024-02-21 20:50:01 +0000

    Add: Cleaning up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import Log from "./components/Log";
 import GameOver from "./components/GameOver";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
-const initialGameBoard = [
+const PLAYERS = {
+	X: "Player 1",
+	O: "Player 2",
+};
+
+const INITIAL_GAME_BOARD = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null],
@@ -21,14 +26,8 @@ function deriveActivePlayer(gameTurns) {
 	return currentPlayer;
 }
 
-function App() {
-	const [players, setPlayers] = useState({ X: "Player 1", O: "Player 2" });
-	const [gameTurns, setGameTurns] = useState([]);
-	// const [activePlayer, setActivePlayer] = useState("X");
-
-	const activePlayer = deriveActivePlayer(gameTurns);
-
-	let gameBoard = [...initialGameBoard.map((array) => [...array])];
+function deriveGameBoard(gameTurns) {
+	let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
 
 	for (const turn of gameTurns) {
 		const { square, player } = turn;
@@ -36,6 +35,10 @@ function App() {
 
 		gameBoard[row][col] = player;
 	}
+	return gameBoard;
+}
+
+function deriveWinner(gameBoard, player) {
 	let winner = null;
 
 	for (const combination of WINNING_COMBINATIONS) {
@@ -51,14 +54,25 @@ function App() {
 			firstSquareSymbol === secondSquareSymbol &&
 			firstSquareSymbol === thirdSquareSymbol
 		) {
-			winner = players[firstSquareSymbol];
+			winner = player[firstSquareSymbol];
 		}
 	}
+	return winner;
+}
+
+function App() {
+	const [players, setPlayers] = useState(PLAYERS);
+	const [gameTurns, setGameTurns] = useState([]);
+
+	const activePlayer = deriveActivePlayer(gameTurns);
+
+	const gameBoard = deriveGameBoard(gameTurns);
+
+	const winner = deriveWinner(gameBoard, players);
 
 	const hasDraw = gameTurns.length === 9 && !winner;
 
 	function handleSelectSquare(rowIndex, colIndex) {
-		// setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
 		setGameTurns((prevTurns) => {
 			const currentPlayer = deriveActivePlayer(prevTurns);
 
@@ -85,13 +99,13 @@ function App() {
 			<div id="game-container">
 				<ol id="players" className="highlight-player">
 					<Player
-						initialName="Player 1"
+						initialName={PLAYERS.X}
 						symbol="X"
 						isActive={activePlayer === "X"}
 						onChangeName={handlePlayerNameChange}
 					/>
 					<Player
-						initialName="Player 2"
+						initialName={PLAYERS.O}
 						symbol="O"
 						isActive={activePlayer === "O"}
 						onChangeName={handlePlayerNameChange}
